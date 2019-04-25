fd3fa01c56649a0768514f8d5f8c90190521c779
K <kevin.liu.v19@gmail.com>
2019-04-26 01:52:51 +1000

    Add PHP extensions to Dockerfile and test SQL connection on webapge

--- web/index.php
+++ web/index.php
@@ -1,7 +1,17 @@
 <html>
  <head>
+  <title>PHP Web Page</title>
  </head>
  <body>
   <h1>PHP Web Page</h1>
+  <?php
+   $conn = mysqli_connect('db', 'user', 'pass', "sqlDb");
+   if ($conn->connect_error) {
+    die("Database Connection Error: " . $conn->connect_error);
+   } else {
+    echo "Connected to database</br>";
+   }
+   mysqli_close($conn);
+  ?>
  </body>
 </html>
